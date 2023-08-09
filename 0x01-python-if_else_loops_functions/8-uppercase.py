ce347c999ce5da83bc6efa6ab2be91f5811350e2
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-08-08 20:18:28 -0700

    complete 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(0, len(str)):
+        print("wat")
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             c = chr(ord(str[i]) - 32)
         else:
