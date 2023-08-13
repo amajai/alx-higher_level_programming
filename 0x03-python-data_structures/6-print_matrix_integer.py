0a7f50775cee066f395dd1aa7c8cf0e04ce0204e
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-08-13 05:37:36 -0700

    add digit identifier 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
         if rows > 0:
             for i in range(rows):
                 for j in range(len(matrix[i])):
-                    print("{}".format(matrix[i][j]), end=" ")
+                    print("{:d}".format(matrix[i][j]), end=" ")
                 print("")
