87095740eed2e20af28137bc979210d61b30ee33
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-08-13 13:52:54 -0700

    complete 10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list:
+        new_list = []
+        for n in my_list:
+            new_list.append(True if n % 2 == 0 else False)
+        return new_list
