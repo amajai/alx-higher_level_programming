c9baba5ccd0992fbfae66d75e43144614b78bec2
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-08-14 04:35:53 -0700

    resolve 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,11 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         rows = len(matrix)
-        if rows > 0:
+        if rows > 0 and len(matrix[0]) > 0:
             for i in range(rows):
-                for j in range(len(matrix[i])):
+                cols = len(matrix[i]) - 1
+                for j in range(cols):
                     print("{:d}".format(matrix[i][j]), end=" ")
-                print("")
+                print("{:d}".format(matrix[i][cols]))
+        else:
+            print()
