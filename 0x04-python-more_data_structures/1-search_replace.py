429ca58ab3e3d28c6c49991c75a6389c0e91e9ff
Abdul <aabdulmajeed.isa@gmail.com>
2023-08-16 22:14:13 +0100

    complete 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list:
+        new_list = my_list[:]
+        for i in range(len(new_list)):
+            if new_list[i] == search:
+                new_list[i] = replace
+                break
+        return new_list
+    return my_list
