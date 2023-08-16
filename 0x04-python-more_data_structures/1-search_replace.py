133d42774080a8afd6be3ecb0594c1b45ebfe521
Abdul <aabdulmajeed.isa@gmail.com>
2023-08-16 22:59:31 +0100

    restructure 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
-        new_list = my_list[:]
+    new_list = my_list[:]
+    if new_list:
         for i in range(len(new_list)):
             if new_list[i] == search:
                 new_list[i] = replace
                 break
-        return new_list
-    return my_list[:]
+    return new_list
