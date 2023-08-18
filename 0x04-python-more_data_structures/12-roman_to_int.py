8e2343e43f66e5cc76244009883db93861f32ea0
Abdul <aabdulmajeed.isa@gmail.com>
2023-08-18 12:09:18 +0100

    complete 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+
+    roman_num = {
+        'I': 1,
+        'IV': 4,
+        'V': 5,
+        'IX': 9,
+        'X': 10,
+        'XL': 40,
+        'L': 50,
+        'XC': 90,
+        'C': 100,
+        'CD': 400,
+        'D': 500,
+        'CM': 900,
+        'M': 1000
+    }
+    if len(roman_string) >= 2:
+        pos = -2
+    else:
+        pos = -1
+    sum = 0
+    while roman_string:
+        get_roman_num = roman_num.get(roman_string[pos:])
+        if get_roman_num:
+            sum += get_roman_num
+            roman_string = roman_string[:pos]
+            pos = -2
+        else:
+            pos += 1
+            if(pos > -1):
+                break
+    return sum
