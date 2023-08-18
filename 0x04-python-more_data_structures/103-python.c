ab487703ccef792f0e040f29bc56ab788b7a15bc
Abdul <aabdulmajeed.isa@gmail.com>
2023-08-18 17:28:54 +0100

    add code in print_python_list

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,6 @@
 #include "Python.h"
 #include "lists.h"
+#include <string.h>
 
 /**
 * print_python_list - prints some basic info about Python lists.
@@ -9,16 +10,18 @@
 */
 void print_python_list(PyObject *p)
 {
-    Py_ssize_t size, w;
+	Py_ssize_t size, w;
 	PyListObject *pl;
+	PyBytesObject *pb;
 	const char *type;
 	Py_ssize_t i;
 
 
-    if (p == NULL)
+	if (p == NULL)
 		return;
 	pl = (PyListObject *)p;
-    size = PyList_Size(p);
+	pb = (PyListObject *)p;
+	size = PyList_Size(p);
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %zd\n", size);
 	printf("[*] Allocated = %zd\n", pl->allocated);
@@ -26,6 +29,12 @@ void print_python_list(PyObject *p)
 	{
 		type = Py_TYPE(pl->ob_item[i])->tp_name;
 		printf("Element %zd: %s\n", i, type);
+		if (strcmp(type, "bytes") == 0)
+		{
+			printf("[.] bytes object info\n");
+			printf("size: %zd\n", pl->ob_item[i]->ob_refcnt);
+
+		}
 	}
 }
 
@@ -37,5 +46,5 @@ void print_python_list(PyObject *p)
 */
 void print_python_bytes(PyObject *p)
 {
-    printf("test\n");
+	printf("test\n");
 }
