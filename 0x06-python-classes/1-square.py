a9342e6674c4f0330c2f1e09e66c9f53fea00aa6
Abdul <aabdulmajeed.isa@gmail.com>
2023-08-30 03:35:07 +0100

    complete 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A Square Module
+
+"""
+
+
+class Square:
+    """A Square class
+
+    """
+    def __init__(self, size):
+        """Initialize instances of Square
+
+        Args:
+          size (int): Define size.
+        """
+        self.__size = size
