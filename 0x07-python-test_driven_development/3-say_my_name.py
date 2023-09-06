d2274ab272cfc27ed9cf79c1160b47091074e9a7
Abdul <aabdulmajeed.isa@gmail.com>
2023-09-06 01:34:38 +0100

    complete 3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A module provides a function to print a person's name based on their
+first and last names.
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Print a person's name based on their first and last names.
+
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
