e9ff11ac81481c1bf8c6e88e35bdeb13e990624d
Abdul <aabdulmajeed.isa@gmail.com>
2023-09-12 23:19:43 +0100

    complete 0x0B/10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module has class (Student) with attributes
+
+"""
+
+
+class Student:
+    """
+    Class containing attributes of a student
+    first_name, last_name and age
+
+    """
+    def __init__(self, first_name, last_name, age):
+        """Student instance attributes
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        dict = {}
+        for k in attrs:
+            v = self.__dict__.get(k)
+            if v:
+                dict[k] = v
+        return dict
