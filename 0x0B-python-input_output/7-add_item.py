899c78af9a6b6f44bbcaa6ba2f7da7d9836e4fe5
Abdul <aabdulmajeed.isa@gmail.com>
2023-09-12 22:35:43 +0100

    correct 0x0B/7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,13 +4,15 @@ This module has function script that adds all arguments to a Python list, and
 then save them to a file
 
 """
-import sys, os
+import sys
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 def main(argv=[]):
-    """script that adds all arguments to a Python list, and then save them to a file
+    """script that adds all arguments to a Python list, and then save
+    them to a file
     """
     filename = "add_item.json"
     obj = []
