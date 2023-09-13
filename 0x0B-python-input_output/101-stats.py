7818211e28bed0693308dd03c380bda96c3a13b8
Abdul <aabdulmajeed.isa@gmail.com>
2023-09-13 02:16:36 +0100

    correct 0x0B/101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -9,25 +9,27 @@ import sys
 sum = 0
 count = 0
 codes = {}
-while True:
-    try:
-        for line in sys.stdin:
-            status_code, file_size = line.split('"')[-1].strip().split(" ")
 
-            sum += int(file_size)
-            if codes.get(status_code):
-                codes[status_code] += 1
-            else:
-                codes[status_code] = 1
+if __name__ == "__main__":
+    while True:
+        try:
+            for line in sys.stdin:
+                status_code, file_size = line.split('"')[-1].strip().split(" ")
 
-            count += 1
-            if count == 10:
-                print("File size: {:d}".format(sum))
-                for code, size in sorted(codes.items()):
-                    print("{}: {:d}".format(code, size))
-                count = 0
-    except KeyboardInterrupt:
-        print("File size: {:d}".format(sum))
-        for code, size in sorted(codes.items()):
-            print("{}: {:d}".format(code, size))
-        break
+                sum += int(file_size)
+                if codes.get(status_code):
+                    codes[status_code] += 1
+                else:
+                    codes[status_code] = 1
+
+                count += 1
+                if count == 10:
+                    print("File size: {:d}".format(sum))
+                    for code, size in sorted(codes.items()):
+                        print("{}: {:d}".format(code, size))
+                    count = 0
+        except KeyboardInterrupt:
+            print("File size: {:d}".format(sum))
+            for code, size in sorted(codes.items()):
+                print("{}: {:d}".format(code, size))
+            break
