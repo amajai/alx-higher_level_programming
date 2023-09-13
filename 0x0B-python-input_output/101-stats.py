f927e5ebd308f6d4e5b850d1075ba0904eab320e
Abdul <aabdulmajeed.isa@gmail.com>
2023-09-13 02:07:56 +0100

    complete 0x0B/101-stats.py

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This script that reads stdin line by line and computes metrics
+
+"""
+import sys
+
+
+sum = 0
+count = 0
+codes = {}
+while True:
+    try:
+        for line in sys.stdin:
+            status_code, file_size = line.split('"')[-1].strip().split(" ")
+
+            sum += int(file_size)
+            if codes.get(status_code):
+                codes[status_code] += 1
+            else:
+                codes[status_code] = 1
+
+            count += 1
+            if count == 10:
+                print("File size: {:d}".format(sum))
+                for code, size in sorted(codes.items()):
+                    print("{}: {:d}".format(code, size))
+                count = 0
+    except KeyboardInterrupt:
+        print("File size: {:d}".format(sum))
+        for code, size in sorted(codes.items()):
+            print("{}: {:d}".format(code, size))
+        break
