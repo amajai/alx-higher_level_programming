3e22ba52402a386eca8b6f34ff8ac2a39f720f27
Abdul <aabdulmajeed.isa@gmail.com>
2023-09-15 23:35:33 +0100

    validate Rectangle class attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -44,6 +44,10 @@ class Rectangle(Base):
         Args:
            value (int): Size of width
         """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -62,6 +66,10 @@ class Rectangle(Base):
         Args:
            value (int): Size of height
         """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -80,6 +88,10 @@ class Rectangle(Base):
         Args:
            value (int): Value of x
         """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -98,4 +110,8 @@ class Rectangle(Base):
         Args:
            value (int): Value of y
         """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
