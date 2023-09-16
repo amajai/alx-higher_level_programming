bad64b45534ac159d374b16d6dce9e50f8b45bda
Abdul <aabdulmajeed.isa@gmail.com>
2023-09-16 12:32:25 +0100

    add args and kwarg to update method in Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,9 +21,9 @@ class Square(Rectangle):
             y (int or optional): y value. Default is 0
             id (int or None): id value. Default is None
         """
-        super().__init__(id=id, width=size, height=size, x=x, y=y)
         self.x = x
         self.y = y
+        super().__init__(id=id, width=size, height=size, x=x, y=y)
 
     @property
     def size(self):
@@ -43,6 +43,37 @@ class Square(Rectangle):
         """
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """ assigns new argument value to each attribute
+
+        Args:
+            args (list): list of arguments
+            kwargs (dict): dictionary with key/value
+        """
+        _size = self.size
+        if args:
+            if len(args) > 0:
+                super().__init__(id=args[0], width=_size, height=_size)
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            if kwargs:
+                if kwargs.get("id"):
+                    _id = kwargs.get("id")
+                    _x = self.x
+                    _y = self.y
+                    super().__init__(_size, _size, _x, _y, _id)
+                if kwargs.get("size"):
+                    self.size = kwargs.get("size")
+                if kwargs.get("x"):
+                    self.x = kwargs.get("x")
+                if kwargs.get("y"):
+                    self.y = kwargs.get("y")
 
     def __str__(self):
         xy = "{:d}/{:d}".format(self.x, self.y)
