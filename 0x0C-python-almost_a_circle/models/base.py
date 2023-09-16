24c53b4de51dacb01026fa52b055bd1d98f1fb9a
Abdul <aabdulmajeed.isa@gmail.com>
2023-09-16 13:03:15 +0100

    add to_json_string method to Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 This module is for class Base
 
 """
+import json
 
 
 class Base:
@@ -20,3 +21,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation of list_dictionaries
+        Args:
+            list_dictionaries (list[dict,...]): list of dictionaries
+        """
+        if list_dictionaries:
+            return json.dumps(list_dictionaries)
+        else:
+            return "[]"
