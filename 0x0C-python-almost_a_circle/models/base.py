f7fd87e12188a6f4b7fcb07e75d51434c414ed7b
Abdul <aabdulmajeed.isa@gmail.com>
2023-09-16 13:35:34 +0100

    add save_to_file method to Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,6 +25,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ returns the JSON string representation of list_dictionaries
+
         Args:
             list_dictionaries (list[dict,...]): list of dictionaries
         """
@@ -32,3 +33,15 @@ class Base:
             return json.dumps(list_dictionaries)
         else:
             return "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file
+
+        Args:
+            list_objs (list): list of instances who inherits from Base
+        """
+        path = "{}.json".format(cls.__name__)
+        dict_list = [obj.to_dictionary() for obj in list_objs]
+        with open(path, "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(dict_list))
