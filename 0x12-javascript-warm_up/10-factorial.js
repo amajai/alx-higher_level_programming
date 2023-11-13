8791967f92e23a1aefedc69ed9a0cd22ce6cb496
Abdul <aabdulmajeed.isa@gmail.com>
2023-11-13 12:27:24 +0100

    done

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const num = parseInt(args[0]);
+console.log(factorial(num));
+
+function factorial (n) {
+  if (isNaN(n) || n === 1) {
+    return 1;
+  }
+  return factorial(n - 1) * n;
+}
