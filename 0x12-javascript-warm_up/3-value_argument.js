65f29b207ca77ee15e8611621ef496ce6dbadecf
Abdul <aabdulmajeed.isa@gmail.com>
2023-11-13 12:10:54 +0100

    fix 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-const [, , ...args] = argv;
+const args = process.argv.slice(2);
 if (!args[0]) {
   console.log('No argument');
 } else {
