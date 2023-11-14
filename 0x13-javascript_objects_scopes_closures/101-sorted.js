18f03623ff84c74c1b0e1bb2e9c3bf66efe27904
Abdul <aabdulmajeed.isa@gmail.com>
2023-11-14 16:24:32 +0100

    done 101-sorted.js

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+for (const k in dict) {
+  if (dict[k] in newDict) {
+    newDict[dict[k]].push(k);
+  } else {
+    newDict[dict[k]] = [];
+    newDict[dict[k]].push(k);
+  }
+}
+console.log(newDict);
