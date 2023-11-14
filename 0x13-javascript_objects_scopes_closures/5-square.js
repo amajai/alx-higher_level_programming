5178997f2ca5c1e2247101bc82a07bb5fe4144cd
Abdul <aabdulmajeed.isa@gmail.com>
2023-11-14 14:38:35 +0100

    done 6-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -5,23 +5,6 @@ class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
 }
 
 module.exports = Square;
