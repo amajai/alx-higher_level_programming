12cc55241f24076119b02cc3714b8f76992fac39
Abdul <aabdulmajeed.isa@gmail.com>
2023-11-18 15:10:01 +0100

    done 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module to query the states list from database
+"""
+import MySQLdb
+import sys
+
+def get_states(username, password, database_name):
+    """
+    Get the list of states for database
+    """
+    db = MySQLdb.connect(
+        user=username,
+        passwd=password,
+        db=database_name
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    res = cursor.fetchall()
+    for state in res[:5]:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit()
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    get_states(username, password, database_name)
