7d18c331d2314e539e645c05800b31ca32b574bf
Abdul <aabdulmajeed.isa@gmail.com>
2023-11-19 09:35:25 +0100

    done 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module to query the states list from database
+"""
+import MySQLdb
+import sys
+
+
+def get_states_with_N(username, password, database_name):
+    """
+    Get the list of states for database
+    """
+    db = MySQLdb.connect(
+        user=username,
+        passwd=password,
+        db=database_name
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    res = cursor.fetchall()
+    for state in res:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit()
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    get_states_with_N(username, password, database_name)
