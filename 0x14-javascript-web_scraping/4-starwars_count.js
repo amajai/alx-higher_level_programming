cbe54f67cd329f4d945bb108ea16e587e3b2b6c2
Abdul <aabdulmajeed.isa@gmail.com>
2024-01-03 13:10:43 +0100

    done 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,24 +2,13 @@
 const request = require('request');
 
 const apiUrl = process.argv[2];
-let count = 0;
-const charId = 18;
-const character = `https://swapi-api.alx-tools.com/api/people/${charId}/`;
+let count;
 
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.log(error);
     return;
   }
-  if (response.statusCode === 200) {
-    const data = JSON.parse(body);
-    for (const key of data.results) {
-      for (const charLink of key.characters) {
-        if (charLink === character) {
-          count += 1;
-        }
-      }
-    }
-    console.log(count);
-  }
+  count = body.split('/people/18/').length - 1;
+  console.log(count);
 });
